4160769fa7d6434d07e9e04c44243fa7cccbb1bc
ranjithvenkatesh <mail@ranjithvenkatesh.com>
2014-02-27 18:04:50 +0100

    Initial commit

---
+++ src/SimpleFileVerifier/SimpleFileVerifier/CommandLineOptions.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleFileVerifier
+{
+    using CommandLine;
+    using CommandLine.Text;
+
+    /// <summary>
+    ///     Supported commandline options
+    /// </summary>
+    public class CommandLineOptions
+    {
+        [Option('o', "output", HelpText = "Path to SFV File")]
+        public string Output { get; set; }
+
+        [Option('d', "directory", HelpText = "Path to Directory")]
+        public string Directory { get; set; }
+
+        [Option('f', "file", HelpText = "Path to File")]
+        public string File { get; set; }
+
+        [Option('p', "pattern", HelpText = "Search pattern")]
+        public string Pattern { get; set; }
+
+        [Option('v', "verbose", DefaultValue = true,
+          HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+
+    }
+}
